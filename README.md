ca1ff50607dd91d69fb480b5f6060436bb54c7e7
Thong Nguyen <nxthongbk@gmail.com>
2019-06-06 15:52:00 +0700

    update

---
+++ README.md
@@ -0,0 +1,25 @@
+# Yellow Test
+
+
+
+## Prerequisites
+
+1. mangOH configured as:  
+        WiFi access point  
+        discoverable Bluetooth device  
+1. USB NFC tag reader, to be connected to the PC
+1. IoT Test Card, modified to disable the SDIO switch
+1. Expansion Connector Test Card
+1. SIM
+1. microSD Card
+
+## Legato Application
+------------------
+There are 2 legato applications:
+* ```YellowTestService```: provides api for Testing App.
+* ```YellowTest```: Main test app
+## Setup
+## How To Run
+
+
+
---
+++ YellowTestService/YellowTestServiceComponent/YellowTestServiceComponent.c
@@ -0,0 +1,48 @@
+#include "legato.h"
+#include "interfaces.h"
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Check: SIM State.
+ *
+ */
+//--------------------------------------------------------------------------------------------------
+
+le_result_t yellow_test_CheckSimState(void)
+{
+	le_sim_States_t state;
+	le_result_t res = le_sim_SelectCard(LE_SIM_EXTERNAL_SLOT_1);
+	LE_ASSERT(res == LE_OK);
+
+	state = le_sim_GetState(LE_SIM_EXTERNAL_SLOT_1);
+	if (LE_SIM_READY == state)
+	{
+		return LE_OK;
+	}
+	else
+	{
+		return LE_FAULT;
+	}
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Check: signal quality.
+ *
+ */
+//--------------------------------------------------------------------------------------------------
+le_result_t yellow_test_MeasureSingalstrength
+(
+    uint32_t* qual
+)
+{
+    le_result_t res;
+    res = le_mrc_GetSignalQual(qual);
+    return res;
+}
+
+
+COMPONENT_INIT
+{
+    LE_INFO("YellowTest Service Start.");
+}
